fa3dea77149acd89bf6049f4381182a8c603e578
Jeroen van Kessel <krabbedoelie@hotmail.com>
2019-07-23 16:28:21 +0200

    Create 4iptables.sh

---
+++ 4iptables.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#By krabelize | cryptsus.com
+#iptables base-script
+LOOPBACK="127.0.0.0/8"
+NIC_DATA="eth0"
+NIC_MGMT="eth0"
+SERVER_IP_DATA=$(hostname -I | awk '{print $1}')
+SERVER_IP_MGMT=$(hostname -I | awk '{print $1}')
+LOCAL_NETWORK="192.168.1.1/24"
+DNS1="1.1.1.1"
+DNS2="8.8.8.8"
+
+#Flush all iptables settings
+iptables -F
+iptables -X
+
+#Deny any-any
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT DROP
+
+#Allow loopback traffic
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A INPUT -s $LOOPBACK ! -i lo -j DROP
+
+#Drop invalid packets 
+iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+
+################
+#INPUT rules   #
+################
+#Allow incoming OpenVPN connections to this host
+iptables -A INPUT -i $NIC_MGMT -p udp -s 0/0 -d $SERVER_IP_MGMT --sport 32768:65535 --dport 1043 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o $NIC_MGMT -p udp -s $SERVER_IP_MGMT -d 0/0 --sport 1043 --dport 32768:65535 -m state --state ESTABLISHED -j ACCEPT
+
+#################
+#OUTPUT rules   #
+#################
+#Allow outgoing SSH sessions
+iptables -A OUTPUT -o $NIC_DATA -p tcp -s $SERVER_IP_DATA -d 0/0 --sport 32768:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i $NIC_DATA -p tcp -d $SERVER_IP_DATA -s 0/0 --sport 22 --dport 32768:65535 -m state --state ESTABLISHED -j ACCEPT
+
+#Allow outgoing DNS lookups 
+iptables -A OUTPUT -o $NIC_DATA -p udp -s $SERVER_IP_DATA -d $DNS1 --sport 32768:65535 --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i $NIC_DATA -p udp -s $DNS1 -d $SERVER_IP_DATA --sport 53 --dport 32768:65535 -m state --state ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o $NIC_DATA -p udp -s $SERVER_IP_DATA -d $DNS2 --sport 32768:65535 --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i $NIC_DATA -p udp -s $DNS2 -d $SERVER_IP_DATA --sport 53 --dport 32768:65535 -m state --state ESTABLISHED -j ACCEPT
+
+#Allow outgoing ping requests
+iptables -A OUTPUT -o $NIC_DATA -p icmp --icmp-type 8 -s $SERVER_IP_DATA -d 0/0 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i $NIC_DATA -p icmp --icmp-type 0 -d $SERVER_IP_DATA -s 0/0 -m state --state ESTABLISHED -j ACCEPT
+
+#Allow outgoing HTTP(S) sessions for apt-get update and browsing
+iptables -A OUTPUT -o $NIC_DATA -p tcp -m tcp -s $SERVER_IP_DATA --sport 32768:65535 -d 0/0 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i $NIC_DATA -p tcp -s 0/0 -d $SERVER_IP_DATA --sport 80 --dport 32768:65535 -m state --state ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o $NIC_DATA -p tcp -m tcp -s $SERVER_IP_DATA --sport 32768:65535 -d 0/0 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i $NIC_DATA -p tcp -s 0/0 -d $SERVER_IP_DATA --sport 443 --dport 32768:65535 -m state --state ESTABLISHED -j ACCEPT	
+
+#Allow DHCP handshakes for dynamic IP-address assignment
+iptables -A OUTPUT -o $NIC_DATA -p udp -s $SERVER_IP_DATA -d 0/0 --sport 32768:65535 --dport 68 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i $NIC_DATA -p udp -d $SERVER_IP_DATA -s 0/0 --sport 68 --dport 32768:65535 -m state --state ESTABLISHED -j ACCEPT
+
+#Allow outgoing NTP time sync streams
+iptables -A OUTPUT -o $NIC_DATA -p udp -s $SERVER_IP_DATA -d 0/0 --sport 32768:65535 --dport 123 -m state --state NEW -j ACCEPT
+iptables -A INPUT -i $NIC_DATA -p udp -d $SERVER_IP_DATA -s 0/0 --sport 123 --dport 32768:65535 -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+#Make sure nothing goes in or out from this host
+iptables -A INPUT -j DROP
+iptables -A OUTPUT -j DROP
+
+#Save settings
+sudo sh -c "iptables-save > /sbin/scripts/iptables4.rules"
