48ea12402eef1e6bc05088e5b90dafdd9025ba61
Jeroen van Kessel <krabbedoelie@hotmail.com>
2019-07-23 16:34:00 +0200

    Create 6iptables.sh

---
+++ 6iptables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#By krabelize | cryptsus.com
+#Persistant IPv6 iptables firewall script
+
+#Reset all IPv6 iptables rules
+ip6tables -F
+ip6tables -X
+
+#Disallowing any IPv6 traffic as deny any any 
+ip6tables -P INPUT DROP
+ip6tables -P FORWARD DROP
+ip6tables -P OUTPUT DROP
+
+ip6tables -A INPUT -s ::1/128 ! -i lo -j DROP
+
+#Save IPv6 iptables config
+sudo sh -c "ip6tables-save > /sbin/scripts/iptables6.rules"
